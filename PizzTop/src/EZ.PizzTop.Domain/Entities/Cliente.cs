822330a0843dfeccbe1a5f45b864a488dc477bc2
Edson Zeballos <09edson@gmail.com>
2017-09-25 23:33:15 -0400

    Classes de Cliente e Endereço

---
+++ PizzTop/src/EZ.PizzTop.Domain/Entities/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EZ.PizzTop.Domain.Entities
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            ClienteId = Guid.NewGuid();
+            Enderecos = new List<Endereco>();
+        }
+
+        public Guid ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+        public virtual ICollection<Endereco> Enderecos { get; set; }
+
+    }
+}
