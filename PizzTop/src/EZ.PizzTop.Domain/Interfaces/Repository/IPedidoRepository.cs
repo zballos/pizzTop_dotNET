4e38e9f683ef4d2f43ff38a3401aca2d23941024
Edson Zeballos <09edson@gmail.com>
2017-10-06 23:36:43 -0400

    Interfaces de repositorios para Pedidos e Produtos

---
+++ PizzTop/src/EZ.PizzTop.Domain/Interfaces/Repository/IPedidoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using EZ.PizzTop.Domain.Entities;
+
+namespace EZ.PizzTop.Domain.Interfaces.Repository
+{
+    public interface IPedidoRepository : IRepository<Pedido>
+    {
+        //double CalculaTotalPedido();
+        IEnumerable<Pedido> ObterPedidosPorCliente(Guid clienteId);
+
+        //Pedido ObterPorProduto(Guid guid);
+    }
+}
---
+++ PizzTop/src/EZ.PizzTop.Domain/Interfaces/Repository/IProdutoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using EZ.PizzTop.Domain.Entities;
+
+namespace EZ.PizzTop.Domain.Interfaces.Repository
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        IEnumerable<Produto> ObterProdutosPorTipoProduto(Guid produtoId);
+    }
+}
