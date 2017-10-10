018df828641e08e71a9e62f43d0b5f5ac05aac5c
Edson Zeballos <09edson@gmail.com>
2017-10-09 22:33:56 -0400

    Classes do UnitOfWork

---
+++ PizzTop/src/EZ.PizzTop.Infra.Data/UoW/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using EZ.PizzTop.Infra.Data.Context;
+using EZ.PizzTop.Infra.Data.Interfaces;
+using Microsoft.Practices.ServiceLocation;
+
+namespace EZ.PizzTop.Infra.Data.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly PizzTopContext _dbContext;
+        private bool _disposed;
+
+        // será usado o ServiceLocator como injeção de dependência específica
+        public UnitOfWork()
+        {
+            //var contextManager = new ContextManager().GetContext();
+            var contextManager = ServiceLocator.Current.GetInstance<IContextManager>();
+            _dbContext = contextManager.GetContext();
+        }
+
+        public void BeginTransaction()
+        {
+            _disposed = false;
+        }
+
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _dbContext.Dispose();
+                }
+            }
+
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
