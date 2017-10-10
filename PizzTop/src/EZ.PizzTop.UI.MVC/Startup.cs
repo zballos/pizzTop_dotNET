4347f8670ef4de49125895adfe918c06dddd5286
Edson Zeballos <09edson@gmail.com>
2017-10-09 22:51:29 -0400

    Projeto UI.MVC, apenas criado

---
+++ PizzTop/src/EZ.PizzTop.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EZ.PizzTop.UI.MVC.Startup))]
+namespace EZ.PizzTop.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
