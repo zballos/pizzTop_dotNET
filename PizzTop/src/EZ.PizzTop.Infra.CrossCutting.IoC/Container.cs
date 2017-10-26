27d364ff7ecb99d4f12993ade156d7a50b9583d6
Edson Zeballos <09edson@gmail.com>
2017-10-25 22:53:43 -0300

    criando as classes de injecao de dependencia, usando ninject

---
+++ PizzTop/src/EZ.PizzTop.Infra.CrossCutting.IoC/Container.cs
@@ -0,0 +1,17 @@
+﻿using Ninject;
+
+namespace EZ.PizzTop.Infra.CrossCutting.IoC
+{
+    public class Container
+    {
+        /*public Container()
+        {
+            ServiceLocator.SetLocatorProvider(() => new NinjectServiceLocator(GetModule()));
+        }*/
+
+        public StandardKernel GetModule()
+        {
+            return new StandardKernel(new NinjectModulo(), new NinjectAppModulo());
+        }
+    }
+}
---
+++ PizzTop/src/EZ.PizzTop.Infra.CrossCutting.IoC/NinjectModulo.cs
@@ -0,0 +1,25 @@
+﻿using EZ.PizzTop.Application;
+using EZ.PizzTop.Application.Interface;
+using EZ.PizzTop.Domain.Interfaces.Repository;
+using EZ.PizzTop.Domain.Interfaces.Services;
+using EZ.PizzTop.Domain.Services;
+using EZ.PizzTop.Infra.Data.Repository;
+using Ninject.Modules;
+
+namespace EZ.PizzTop.Infra.CrossCutting.IoC
+{
+    public class NinjectModulo : NinjectModule
+    {
+        public override void Load()
+        {
+            // app
+            Bind<IClienteAppService>().To<ClienteAppService>();
+
+            // domain
+            Bind<IClienteService>().To<ClienteService>();
+
+            // data
+            Bind<IClienteRepository>().To<ClienteRepository>();
+        }
+    }
+}
